058ac23d4155b08cabcdb93d3f88430becf471b0
HarrisQs <harris32916@gmail.com>
2016-04-10 13:29:38 +0800

    C:/Program Files/Git

--- MergeDataAndDoc/Test.cs
+++ MergeDataAndDoc/Test.cs
@@ -21,5 +21,15 @@ namespace MergeDataAndDoc
             Program.MergeData(input, template, output);
             Assert.That(output.ToString(), Is.EqualTo("張先生(身份證字號A445)為本校專任教師，聘期9。\r\n\t\t\t\t\t\t\t此聘                  \r\n\t\t\t\t\t\t\t\t校長\r\n\r\n"));
         }
+        [Test]
+        public void MergeTest2()
+        {
+            StringReader input = new StringReader("身份證字號\t中文姓名\t年數\r\n" +
+                                                  "A789455\t張非王\t90");
+            StringReader template = new StringReader("${中文姓名}先生(身份證字號${身份證字號})為本校專任教師，聘期${年數}。\r\n\t\t\t\t\t\t\t此聘                  \r\n\t\t\t\t\t\t\t\t校長\r\n");
+            StringWriter output = new StringWriter();
+            Program.MergeData(input, template, output);
+            Assert.That(output.ToString(), Is.EqualTo("張非王先生(身份證字號A789455)為本校專任教師，聘期90。\r\n\t\t\t\t\t\t\t此聘                  \r\n\t\t\t\t\t\t\t\t校長\r\n\r\n"));
+        }
     }
 }
